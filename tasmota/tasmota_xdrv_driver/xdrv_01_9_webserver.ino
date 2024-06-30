2d53a1418b8bf04c1974eb9806546d04b38a2150
Richard Kircheis <tinyisland@web.de>
2024-06-30 20:05:07 +0200

    Added my info to Webserver-page footer

--- tasmota/tasmota_xdrv_driver/xdrv_01_9_webserver.ino
+++ tasmota/tasmota_xdrv_driver/xdrv_01_9_webserver.ino
@@ -408,7 +408,8 @@ const char HTTP_COUNTER[] PROGMEM =
   "<br><div id='t' style='text-align:center;'></div>";
 
 const char HTTP_END[] PROGMEM =
-  "<div style='text-align:right;font-size:11px;'><hr/><a href='https://bit.ly/tasmota' target='_blank' style='color:#aaa;'>Tasmota %s %s " D_BY " Theo Arends</a></div>"
+  "<div style='text-align:right;font-size:11px;'><hr/><a href='https://bit.ly/tasmota' target='_blank' style='color:#aaa;'>Tasmota %s %s " D_BY " Theo Arends</a>"
+  "<br><a href='https://tiny-data.org' target='_blank' style='color:#aaa;'>Flashed by www.tiny-data.org</a></div>"
   "</div>"
   "</body>"
   "</html>";
